9397d2a24b5426cd02bda7eb1e67b7689d47ec8d
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-13 14:44:32 -0500

    2 files modified, 1 added

---
+++ app.js
@@ -0,0 +1,7 @@
+// add the button in javscript
+const button = document.getElementById('submit-button').classList;
+
+// adding classlist you have to use classlist
+function animation() {
+  button.add('animationButton');
+}
